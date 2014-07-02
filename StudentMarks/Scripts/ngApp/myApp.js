7a6d8ff0be14367f8711e2b59426cf54c58ade62
dagilleland <dagilleland@shaw.ca>
2014-07-02 11:25:01 -0600

    AngularJS App (stub)

---
+++ StudentMarks/Scripts/ngApp/myApp.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+
+    var id = 'myApp';
+
+    // TODO: Inject modules as needed.
+    var myApp = angular.module('myApp', [
+        // Angular modules 
+        'ngAnimate',        // animations
+        'ngRoute'           // routing
+
+        // Custom modules 
+
+        // 3rd Party Modules
+        
+    ]);
+
+    // Execute bootstrapping code and any dependencies.
+    // TODO: inject services as needed.
+    myApp.run(['$q', '$rootScope',
+        function ($q, $rootScope) {
+
+        }]);
+})();
