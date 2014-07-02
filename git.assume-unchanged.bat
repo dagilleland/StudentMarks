8c4e5eaa9860bb294f65dd6fc310e688286d0bbf
dagilleland <dagilleland@shaw.ca>
2014-07-02 11:14:13 -0600

    Proof-of-concept Jasmine Testing in VS2013

---
+++ git.assume-unchanged.bat
@@ -0,0 +1,4 @@
+git update-index --assume-unchanged "StudentMarks\App_Data\aspnet-StudentMarks-2014070
+2061805.mdf"
+git update-index --assume-unchanged "StudentMarks\App_Data\aspnet-StudentMarks-2014070
+2061805_log.ldf"
