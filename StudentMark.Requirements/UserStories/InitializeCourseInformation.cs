6ade2e3f31a25bac4c0086f71b941757a062c73e
dagilleland <dagilleland@shaw.ca>
2014-07-09 09:56:01 -0600

    Added Acceptance Tests

---
+++ StudentMark.Requirements/UserStories/InitializeCourseInformation.cs
@@ -0,0 +1,72 @@
+using StudentMark.Requirements.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.BDDfy;
+using TestStack.BDDfy.Core;
+using TestStack.BDDfy.Scanners.StepScanners.Fluent;
+using Xunit;
+using Xunit.Extensions;
+using StudentMarks.Models.Entities;
+using StudentMarks.Controllers;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace StudentMark.Requirements.UserStories
+{
+    [Story(AsA = Actor.INSTRUCTOR,
+               IWant = "To enter the mark structure for the course",
+               SoThat = "I have the basic framework to enter student marks")]
+    public class InitializeCourseInformation
+    {
+        #region Primary - Enter Evaluation Components
+        [Fact]
+        [AutoRollback]
+        public void EnterEvaluationComponents()
+        {
+            this.Given(_ => GivenNoEvaluationComponentInformationHasBeenEntered())
+                .And(_ => GivenEvaluationComponentDataToBeEntered())
+                .When(_ => WhenIAddEvaluationComponents())
+                .Then(_ => ThenICanRetrieveEvaluationComponentsForTheCourse())
+                .BDDfy();
+        }
+        private void GivenNoEvaluationComponentInformationHasBeenEntered()
+        {
+            throw new NotImplementedException();
+        }
+        private Task GivenEvaluationComponentDataToBeEntered()
+        {
+            throw new NotImplementedException();
+        }
+        private Task WhenIAddEvaluationComponents()
+        {
+            throw new NotImplementedException();
+        }
+        private Task ThenICanRetrieveEvaluationComponentsForTheCourse()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Primary - Enter Course Name
+        [Fact]
+        [AutoRollback]
+        public void SetCourseName()
+        {
+            this.When(_ => WhenIEnterACourseName("Networking 101"))
+                .Then(_ => ThenTheCourseNameIsRecorded("Networking 101"))
+                .BDDfy();
+        }
+        private void WhenIEnterACourseName(string name)
+        {
+            throw new NotImplementedException();
+        }
+        private Task ThenTheCourseNameIsRecorded(string expectedName)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ StudentMark.Requirements/UserStories/ViewClassMarks.cs
@@ -0,0 +1,54 @@
+using StudentMark.Requirements.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.BDDfy;
+using TestStack.BDDfy.Core;
+using TestStack.BDDfy.Scanners.StepScanners.Fluent;
+using Xunit;
+using Xunit.Extensions;
+using StudentMarks.Models.Entities;
+using StudentMarks.Controllers;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace StudentMark.Requirements.UserStories
+{
+    [Story(AsA = Actor.INSTRUCTOR, IWant = "To view marks for the whole class", SoThat = "I can see the overall progress of my students", Title = "View Class Marks")]
+    public class ViewClassMarks
+    {
+        [Fact]
+        [AutoRollback]
+        public void GetClassMarks()
+        {
+            this.Given(_ => GivenAnExistingClassList())
+                .And(_ => GivenAnExistingMarkStructure())
+                .And(_ => GivenMarksHaveBeenEntered())
+                .When(_ => WhenIRequestMarks())
+                .Then(_ => ThenClassMarksAreRetrievedForViewing())
+                .BDDfy();
+        }
+        private void GivenAnExistingClassList()
+        {
+            throw new NotImplementedException();
+        }
+        private Task GivenAnExistingMarkStructure()
+        {
+            throw new NotImplementedException();
+        }
+        private Task GivenMarksHaveBeenEntered()
+        {
+            throw new NotImplementedException();
+        }
+        private Task WhenIRequestMarks()
+        {
+            throw new NotImplementedException();
+        }
+        private Task ThenClassMarksAreRetrievedForViewing()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
