4a9220355c009678cbc219cdb3319332b9393f4f
dagilleland <dagilleland@shaw.ca>
2015-01-24 13:53:56 -0700

    Rebooting

---
+++ Reboot/CourseEvaluation/EvaluationComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentMarks.Framework.CourseEvaluation
+{
+    public class EvaluationComponent
+    {
+        public string Name { get; private set; }
+        public int Weight { get; private set; }
+        public EvaluationComponent(string name, int weight)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("A name is required for an evaluation component");
+            if (weight <= 0 || weight > 100)
+                throw new ArgumentException("A weight between 1 and 100 is required for an evaluation component");
+
+            Name = name;
+            Weight = weight;
+        }
+    }
+}
--- Reboot/Specifications/CourseEvaluation/UnitTest/CourseEvaluationDomain.cs
+++ Reboot/Specifications/CourseEvaluation/UnitTest/CourseEvaluationDomain.cs
@@ -1,13 +1,74 @@
-﻿using System;
+﻿using StudentMarks.Framework.CourseEvaluation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
 
 namespace StudentMarks.Framework.Specifications.CourseEvaluation.UnitTest
 {
     public class CourseEvaluationDomain
     {
+        /* In the Course Evaluation bounded context, the domain
+         *
+         * A_Course
+         *  Should_Require_A_Name
+         *  Should_Require_A_Number
+         *  Should_Reject_Duplicate_Name
+         *  Should_Reject_Duplicate_Number
+         *  Should_Set_Evaluation_Components
+         *  Should_Be_Deliverable_If_Components_Total_100
+         *
+         *      0 < Weight <= 100
+         *
+         */
 
+        public class Course
+        {
+        }
+
+        public class Evaluation_Component_Should
+        {
+            [Theory]
+            [InlineData("Quiz 1")]
+            [InlineData("Lab 1")]
+            public void Require_A_Name(string name)
+            {
+                var sut = new EvaluationComponent(name, 10);
+                Assert.Equal(name, sut.Name);
+            }
+
+            [Theory]
+            [InlineData("")]
+            [InlineData(null)]
+            [InlineData("\t")]
+            [InlineData("\n")]
+            [InlineData(" ")]
+            public void Reject_Invalid_Name(string name)
+            {
+                Assert.Throws<ArgumentException>(() => new EvaluationComponent(name, 10));
+            }
+
+            [Theory]
+            [InlineData(1)]
+            [InlineData(100)]
+            [InlineData(77)]
+            public void Require_A_Weight(int weight)
+            {
+                var sut = new EvaluationComponent("Quiz 1", weight);
+                Assert.Equal(weight, sut.Weight);
+            }
+
+            [Theory]
+            [InlineData(0)]
+            [InlineData(101)]
+            [InlineData(-1)]
+            public void Reject_Invalid_Weight(int weight)
+            {
+                Assert.Throws<ArgumentException>(() => new EvaluationComponent("Quiz 1", weight));
+            }
+        }
     }
 }
