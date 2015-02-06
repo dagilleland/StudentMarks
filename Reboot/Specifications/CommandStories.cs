d767cd9ffc39b89782e2877f3dafd6091484fe2b
dagilleland <dagilleland@shaw.ca>
2015-02-05 17:22:24 -0700

    Beginning CQRS

---
+++ Reboot/Specifications/CommandStories.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentMarks.Framework.Specifications
+{
+    class CommandStories
+    {
+        /*  Testing Commands.
+         *  Given a command, it should
+         *      - Have a handler
+         *      - Modify a Domain Aggregate
+         *      - Produce certain event(s)
+         *      
+         */
+    }
+}
