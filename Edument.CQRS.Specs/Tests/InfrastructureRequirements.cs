4d1eb8da0273fd2c675b504b830476ae25f17a6a
dagilleland <dagilleland@shaw.ca>
2015-02-09 00:46:33 -0700

    Articulating the struggle in doing CQRS
    Doing CQRS and Event Sourcing for the first time all on your own is
    tough. Sure, you might be a master of your programming language, but if
    you're "raised" in a bunch of CRUD models, it can be hard to make the
    transition.
    
    Developers can often gravitate towards "frameworks" to help them with
    their development. For CRUD, there are quite a few choices (thing Entity
    Framework or nHibernate).
    
    In this commit, I am trying to articulate ideas that can "disentangle"
    CQRS and Event Sourcing from the development of the Domain, all with a
    mind towards better leveraging BDD and TDD style tests that cleanly
    distinguishes between testing the Domain (the heart of the app) and
    testing how the Domain interacts with the infrastructure that supports
    it.

--- Edument.CQRS.Specs/Tests/InfrastructureRequirements.cs
+++ Edument.CQRS.Specs/Tests/InfrastructureRequirements.cs
@@ -7,6 +7,23 @@ using System.Threading.Tasks;
 namespace Edument.CQRS.Specs.Tests
 {
     /* Objective: DIST - Domain/Infrastructure Segregation Tests
+
+     *
+     *  Imagine the idea of "Unobtrusive CQRS and Event Sourcing".
+     *
+     *  Domain Driven Design (DDD) and Behaviour Driven Development (BDD) are two highly complementary approaches to software development. Both of them offer techniques and practices that can shape an application into exactly "what the customer ordered" by illuminating the "nature" of the application, that is, its Domain. But taking this understanding of the Domain and breathing life into it by making a working application is no small task. A software application is much more than just the Domain - there's the whole question of the infrastructure that supports the Domain.
+     *
+     * In performing DDD and BDD, software developers have to tackle questions around connecting the Domain
+     *  to the "outside" world, and these questions often revolve around two primary aspects:
+     *      - The "front-end" of the application, which is often the primary source of input and ultimate target of output.
+     *      - The "back-end" of the application, which must be concerned with persisting information critical to the domain.
+     *
+     *  For the back-end, the main persistence choices are to do state-based persistence (as in most CRUD systems) or event-based persistence (also known as Event Sourcing).
+     *  On the front end, BDD may reveal that the Domain (with the help of a bit of DTOs) is mostly sufficient for both input and output. Alternatively, a BDD analysis might reveal that CQRS would be more appropriate.
+     *  
+     *  Resolving the questions of what to use in the back-end and the front-end bring with it implications for the infrastructure of the application as a whole, and it is not unusual to see the design of the Domain as directly affected by those decisions. As these infrastructure aspects become more intertwined with the Domain itself, it's also not uncommon to see the whole design of the system slowly devolve into a "big ball of mud".
+     *
+
      *  Refine the Edument CQRS infrastructure to achieve the following:
      *  - Establish testing patterns that make us of both
      *      - In-Memory Event Stores & Repositories
