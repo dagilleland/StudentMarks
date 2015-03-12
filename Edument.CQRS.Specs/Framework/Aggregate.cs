9ce332874f0111d903c523d2d39c41fbfb5a68e5
dagilleland <dagilleland@shaw.ca>
2015-03-12 10:14:51 -0600

    Put Edument code inside specs project
    To refactor the code, use a copy of Edument code to re-do a basic
    framework.

---
+++ Edument.CQRS.Specs/Framework/Aggregate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Edument.CQRS
+{
+    /// <summary>
+    /// Aggregate base class, which factors out some common infrastructure that
+    /// all aggregates have (ID and event application).
+    /// </summary>
+    public class Aggregate
+    {
+        /// <summary>
+        /// The number of events loaded into this aggregate.
+        /// </summary>
+        public int EventsLoaded { get; private set; }
+
+        /// <summary>
+        /// The unique ID of the aggregate.
+        /// </summary>
+        public Guid Id { get; internal set; }
+
+        protected void ApplyId(Guid aggregateId)
+        {
+            if (Id != Guid.Empty)
+                throw new InvalidOperationException(string.Format(
+                    "Unable to assign new Aggregate Id of {1} - aggregate Id of {0} has already assigned",
+                    Id, aggregateId));
+            Id = aggregateId;
+        }
+
+        public void RebuildFromEvents(Guid aggregateId, IEnumerable events)
+        {
+            ApplyId(aggregateId);
+            ApplyEvents(events);
+        }
+
+        /// <summary>
+        /// Enumerates the supplied events and applies them in order to the aggregate.
+        /// </summary>
+        /// <param name="events"></param>
+        public void ApplyEvents(IEnumerable events)
+        {
+            foreach (var e in events)
+                GetType().GetMethod("ApplyOneEvent")
+                    .MakeGenericMethod(e.GetType())
+                    .Invoke(this, new object[] { e });
+        }
+
+        /// <summary>
+        /// Applies a single event to the aggregate.
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>
+        /// <param name="ev"></param>
+        public void ApplyOneEvent<TEvent>(TEvent ev)
+        {
+            var applier = this as IApplyEvent<TEvent>;
+            if (applier == null)
+                throw new InvalidOperationException(string.Format(
+                    "Aggregate {0} does not know how to apply event {1}",
+                    GetType().Name, ev.GetType().Name));
+            applier.Apply(ev);
+            EventsLoaded++;
+        }
+    }
+}
